87e11471e5c4069f04a63f353b278787babd0c0c
VietAnh30 <vietanhdang487@gmail.com>
2025-03-05 11:59:54 +0700

    fix: migrate product variation

--- database/migrations/2025_02_27_015655_create_variation_attributes_table.php
+++ database/migrations/2025_02_27_015655_create_variation_attributes_table.php
@@ -1,57 +1,32 @@
 <?php
 
-namespace App\Models;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-
-/**
- * Class VariationAttribute
- *
- * @package App\Models
- *
- * @property int $id
- * @property int $product_variation_id
- * @property int $attribute_value_id
- * @property String $created_at
- * @property String $updated_at
- *
- * @property ProductVariation $variation
- * @property AttributeValue $attributeValue
- */
-class VariationAttribute extends Model
-{
-    use HasFactory;
-
+return new class extends Migration {
     /**
-     * Tên bảng trong cơ sở dữ liệu
+     * Run the migrations.
      *
-     * @var string
+     * @return void
      */
-    protected $table = 'variation_attributes';
-
-    /**
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = ['product_variation_id', 'attribute_value_id'];
-
-    /**
-     *
-     * @return BelongsTo
-     */
-    public function variation(): BelongsTo
+    public function up()
     {
-        return $this->belongsTo(ProductVariation::class, 'product_variation_id');
+        Schema::create('variation_attributes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_variation_id')->constrained('product_variations')->onDelete('cascade');
+            $table->foreignId('attribute_value_id')->constrained('attribute_values')->onDelete('cascade');
+            $table->timestamps();
+        });
     }
 
     /**
+     * Reverse the migrations.
      *
-     * @return BelongsTo
+     * @return void
      */
-    public function attributeValue(): BelongsTo
+    public function down()
     {
-        return $this->belongsTo(AttributeValue::class, 'attribute_value_id');
+        Schema::dropIfExists('variation_attributes');
     }
-}
+};
